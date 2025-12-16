e68488f2b8f40322738dc22d29c83b07f7c540a6
Peter Newstein <peternewstein@gmail.com>
2025-12-16 10:35:06 -0800

    :feat added a napari reader function
    dragging the right csv file will allow you to load the data into
    Count3D. The points will be added to the existing Count3D widget if
    possible, or else, it will create a new one.
    
    closes #22
    issue #24 still stands, but the severity is low

---
+++ src/napari_3d_counter/_reader.py
@@ -0,0 +1,50 @@
+"""
+Contains the plugin to read csv files
+"""
+
+from typing import Union, Sequence, Callable, List, Optional
+from warnings import warn
+
+from napari.types import LayerData
+import napari
+import pandas as pd
+import numpy as np
+from ._widget import CellTypeConfig, Count3D
+
+PathLike = str
+PathOrPaths = Union[PathLike, Sequence[PathLike]]
+ReaderFunction = Callable[[PathOrPaths], List[LayerData]]
+
+
+def get_reader(path: "PathOrPaths") -> Optional["ReaderFunction"]:
+    # If we recognize the format, we return the actual reader function
+    print(path)
+    if isinstance(path, str) and path.endswith(".csv"):
+        with open(path, "r") as file:
+            first_line = file.readline()
+        if first_line == "cell_type,z,y,x\n":
+            return csv_file_reader
+    warn(
+        "Invalid CSV file. Use one created by the Save Cells button in Count3D"
+    )
+    return None
+
+
+def csv_file_reader(path: "PathOrPaths") -> List[tuple[None]]:
+    data = pd.read_csv(path)
+    labels = np.unique(data["cell_type"])
+    viewer = napari.current_viewer()
+    if viewer is None:
+        raise RuntimeError("Could not access viewer")
+    try:
+        c3d = next(
+            w
+            for w in viewer.window.dock_widgets.values()
+            if isinstance(w, Count3D)
+        )
+    except StopIteration:
+        c3d = Count3D(viewer, [CellTypeConfig(name=n) for n in labels])
+        viewer.window.add_dock_widget(c3d)
+    # add all of the points
+    c3d.read_points_from_df(data)
+    return [(None,)]
---
+++ src/napari_3d_counter/_tests/test_reader.py
@@ -0,0 +1,62 @@
+import napari
+from napari_3d_counter._widget import get_n3d_counter
+import pytest
+
+from pathlib import Path
+
+HERE = Path(__file__).parent
+
+
+def test_launch_count_3d(make_napari_viewer):
+    viewer: napari.Viewer = make_napari_viewer()
+    viewer.open(HERE / "test_point.csv", plugin="napari-3d-counter")
+    c3d = get_n3d_counter(viewer)
+    df = c3d.save_points_to_df()
+    assert len(df) == 3
+
+
+def test_ignores_bad_csv(make_napari_viewer):
+    viewer: napari.Viewer = make_napari_viewer()
+    with pytest.raises(ValueError):
+        viewer.open(HERE / "bad.csv", plugin="napari-3d-counter")
+
+
+@pytest.mark.skip(reason="wait for #24")
+def test_chooses_right_viewer_first(make_napari_viewer):
+    v1 = make_napari_viewer()
+    v2 = make_napari_viewer()
+    napari.current_viewer()
+    from napari._qt.qt_main_window import _QtMainWindow
+
+    viewer_index = next(
+        i
+        for i, w in enumerate(_QtMainWindow._instances)
+        if v1 is w._qt_viewer.viewer
+    )
+    right_window = _QtMainWindow._instances.pop(viewer_index)
+    _QtMainWindow._instances.append(right_window)
+    v1.open(HERE / "test_point.csv", plugin="napari-3d-counter")
+    c3d1 = get_n3d_counter(v1)
+    df1 = c3d1.save_points_to_df()
+    assert len(df1) == 3
+    c3d2 = get_n3d_counter(v2)
+    df2 = c3d2.save_points_to_df()
+    assert len(df2) == 0
+
+
+def test_attach_to_current_c3d(make_napari_viewer):
+    viewer: napari.Viewer = make_napari_viewer()
+    c3d = get_n3d_counter(viewer)
+    df = c3d.save_points_to_df()
+    assert len(df) == 0
+    viewer.open(HERE / "test_point.csv", plugin="napari-3d-counter")
+    df = c3d.save_points_to_df()
+    assert len(df) == 3
+
+
+def test_empty_csv(make_napari_viewer):
+    viewer: napari.Viewer = make_napari_viewer()
+    viewer.open(HERE / "test_empty.csv", plugin="napari-3d-counter")
+    c3d = get_n3d_counter(viewer)
+    df = c3d.save_points_to_df()
+    assert len(df) == 0
